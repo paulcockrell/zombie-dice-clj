a9ec3bdd69139741da41314769ca99fae645a3cd
Paul Cockrell <paul.cockrell@oxfordmedicalsimulation.com>
2025-03-22 15:01:33 +0000

    Add kaplay wrapper

---
+++ src/main/zombiedice/entities/dice.clj
@@ -0,0 +1,1 @@
+(ns zombiedice.entities.dice)
--- src/main/zombiedice/frontend/app.cljs
+++ src/main/zombiedice/frontend/app.cljs
@@ -1,4 +1,17 @@
-(ns zombiedice.frontend.app)
+(ns zombiedice.frontend.app
+  (:require [zombiedice.frontend.kaplay :as kaplay]))
 
-(defn init []
+(def config
+  {:width 1920
+   :height 1080
+   :letterbox true
+   :background [0 0 0]
+   :global false
+   :touchToMouse true
+   :buttons {:roll {:keyboard ["space"] :mouse "left"}}
+   :debugKey "d"
+   :debug true})
+
+(defn init "Initialize the game" []
+  (kaplay/init config)
   (.log js/console "Zombie Dice initialized"))
