f6d89101a96fcb2f0f5ea9eca595d660263398d1
Paul Cockrell <paul.cockrell@oxfordmedicalsimulation.com>
2025-04-12 20:40:01 +0100

    Add player input and validation

--- src/main/zombiedice/frontend/app.cljs
+++ src/main/zombiedice/frontend/app.cljs
@@ -28,12 +28,29 @@
 (defn get-current-dice [game-state]
   (:current-dice game-state))
 
+(defn invalid-name-size? [name]
+  (or
+   (< (count name) 2)
+   (> (count name) 10)))
+
+(defn name-taken? [game-state name]
+  (some (partial = name) (get-players game-state)))
+
+(defn valid-name? [game-state name]
+  (cond
+    (invalid-name-size? name)
+    (name-taken? game-state name)
+    :else true))
+
 (defn add-player
   "Add player to the game states players key"
   [game-state name]
-  (prn (str "Adding player " name))
-  (let [players (get-players game-state)]
-    (assoc game-state :players (conj players (player/init-player name)))))
+  (if (valid-name? game-state name)
+    (let [players (get-players game-state)]
+      (prn (str "Adding player " name))
+      (assoc game-state :players (conj players (player/init-player name))))
+    (do (prn "Invalid name. Must be between 2 and 10 characters and not be taken.")
+        game-state)))
 
 (defn add-dice
   ([game-state dice]
@@ -140,8 +157,6 @@
   [game-state]
   (let [new-game-state
         (-> initial-game-state
-            (add-player "Paul")
-            (add-player "Bob")
             (add-dice (dice/init-dice)))]
     (save-game-state! game-state new-game-state)))
 
@@ -194,11 +209,15 @@
    "Restart"])
 
 (defn yield-turn-btn [game-state]
-  [:button.button.is-warning {:on-click (fn [] (yield-turn! game-state))}
+  [:button.button.is-warning {:on-click
+                              (fn []
+                                (yield-turn! game-state))
+                              :disabled
+                              (= 0 (count (get-players @game-state)))}
    "Yield turn"])
 
 (defn roll-dice-btn [game-state]
-  [:button.button.is-primary {:on-click (fn [] (play-hand! game-state))}
+  [:button.button.is-primary {:on-click (fn [] (play-hand! game-state)) :disabled (= 0 (count (get-players @game-state)))}
    "Roll dice..."])
 
 (defn navbar-component []
@@ -280,6 +299,9 @@
 (defn current-player-component [game-state]
   [:div
    {:class "column is-12 is-4-desktop"}
+   [:h2
+    {:class "is-size-4 has-text-weight-bold mb-2"}
+    "Current round stats"]
    [:div
     {:class "mb-6 is-flex"}
     [:span
@@ -347,83 +369,78 @@
       {:class "subtitle has-text-grey"}
       (str (get-shots @game-state))]]]])
 
-(defn lorum-component []
+(defn update-player-list [game-state new-player]
+  (let [new-game-state
+        (add-player @game-state @new-player)]
+    (save-game-state! game-state new-game-state)))
+
+(defn player-input [value]
+  [:input {:type "text"
+           :value @value
+           :class "input"
+           :placeholder "Player name"
+           :on-change #(reset! value (-> % .-target .-value))
+           :on-key-press
+           (fn [e]
+             (when (= (.-key e) "Enter")
+               (update-player-list game-state value)
+               (reset! value "")))}])
+
+(defn player-input-save [game-state value]
+  [:button.button.is-primary {:on-click
+                              (fn []
+                                (update-player-list game-state value)
+                                (reset! value ""))}
+   "Add player"])
+
+(defn new-player-component [game-state]
+  (let [name (r/atom "")]
+    (fn []
+      [:div.field.has-addons
+       [:div.control
+        [player-input name]]
+       [:div.control
+        [player-input-save game-state name]]])))
+
+(defn list-players-component [game-state]
   [:div
    {:class "column is-12 is-4-desktop"}
-   [:div
-    {:class "mb-6 is-flex"}
-    [:span
-     [:svg
-      {:width "48",
-       :height "48",
-       :viewBox "0 0 48 48",
-       :fill "none",
-       :xmlns "http://www.w3.org/2000/svg"}
-      [:path
-       {:d
-        "M25.6 22.9C25.7 23 25.8 23 26 23H33C33.6 23 34 22.6 34 22C34 21.8 34 21.7 33.9 21.6L30.4 14.6C30.1 14.1 29.5 13.9 29 14.2C28.9 14.3 28.7 14.4 28.6 14.6L25.1 21.6C24.9 22 25.1 22.6 25.6 22.9ZM29.5 17.2L31.4 21H27.6L29.5 17.2ZM18.5 14C16 14 14 16 14 18.5C14 21 16 23 18.5 23C21 23 23 21 23 18.5C23 16 21 14 18.5 14ZM18.5 21C17.1 21 16 19.9 16 18.5C16 17.1 17.1 16 18.5 16C19.9 16 21 17.1 21 18.5C21 19.9 19.9 21 18.5 21ZM22.7 25.3C22.3 24.9 21.7 24.9 21.3 25.3L18.5 28.1L15.7 25.3C15.3 24.9 14.7 24.9 14.3 25.3C13.9 25.7 13.9 26.3 14.3 26.7L17.1 29.5L14.3 32.3C13.9 32.7 13.9 33.3 14.3 33.7C14.7 34.1 15.3 34.1 15.7 33.7L18.5 30.9L21.3 33.7C21.7 34.1 22.3 34.1 22.7 33.7C23.1 33.3 23.1 32.7 22.7 32.3L19.9 29.5L22.7 26.7C23.1 26.3 23.1 25.7 22.7 25.3ZM33 25H26C25.4 25 25 25.4 25 26V33C25 33.6 25.4 34 26 34H33C33.6 34 34 33.6 34 33V26C34 25.4 33.6 25 33 25ZM32 32H27V27H32V32Z",
-        :fill "#00d1b2"}]
-      [:circle {:cx "24", :cy "24", :r "23.5", :stroke "#00d1b2"}]]]
-    [:div
-     {:class "ml-3"}
-     [:h4
-      {:class "is-size-4 has-text-weight-bold mb-2"}
-      "Lorem ipsum dolor"]
-     [:p
-      {:class "subtitle has-text-grey"}
-      "Pellentesque eu quam vitae mi lacinia consequat quis in metus."]]]
-   [:div
-    {:class "mb-6 is-flex"}
-    [:span
-     [:svg
-      {:width "48",
-       :height "48",
-       :viewBox "0 0 48 48",
-       :fill "none",
-       :xmlns "http://www.w3.org/2000/svg"}
-      [:path
-       {:d
-        "M25.6 22.9C25.7 23 25.8 23 26 23H33C33.6 23 34 22.6 34 22C34 21.8 34 21.7 33.9 21.6L30.4 14.6C30.1 14.1 29.5 13.9 29 14.2C28.9 14.3 28.7 14.4 28.6 14.6L25.1 21.6C24.9 22 25.1 22.6 25.6 22.9ZM29.5 17.2L31.4 21H27.6L29.5 17.2ZM18.5 14C16 14 14 16 14 18.5C14 21 16 23 18.5 23C21 23 23 21 23 18.5C23 16 21 14 18.5 14ZM18.5 21C17.1 21 16 19.9 16 18.5C16 17.1 17.1 16 18.5 16C19.9 16 21 17.1 21 18.5C21 19.9 19.9 21 18.5 21ZM22.7 25.3C22.3 24.9 21.7 24.9 21.3 25.3L18.5 28.1L15.7 25.3C15.3 24.9 14.7 24.9 14.3 25.3C13.9 25.7 13.9 26.3 14.3 26.7L17.1 29.5L14.3 32.3C13.9 32.7 13.9 33.3 14.3 33.7C14.7 34.1 15.3 34.1 15.7 33.7L18.5 30.9L21.3 33.7C21.7 34.1 22.3 34.1 22.7 33.7C23.1 33.3 23.1 32.7 22.7 32.3L19.9 29.5L22.7 26.7C23.1 26.3 23.1 25.7 22.7 25.3ZM33 25H26C25.4 25 25 25.4 25 26V33C25 33.6 25.4 34 26 34H33C33.6 34 34 33.6 34 33V26C34 25.4 33.6 25 33 25ZM32 32H27V27H32V32Z",
-        :fill "#00d1b2"}]
-      [:circle {:cx "24", :cy "24", :r "23.5", :stroke "#00d1b2"}]]]
-    [:div
-     {:class "ml-3"}
-     [:h4
-      {:class "is-size-4 has-text-weight-bold mb-2"}
-      "Lorem ipsum dolor"]
-     [:p
-      {:class "subtitle has-text-grey"}
-      "Pellentesque eu quam vitae mi lacinia consequat quis in metus."]]]
-   [:div
-    {:class "mb-6 is-flex"}
-    [:span
-     [:svg
-      {:width "48",
-       :height "48",
-       :viewBox "0 0 48 48",
-       :fill "none",
-       :xmlns "http://www.w3.org/2000/svg"}
-      [:path
-       {:d
-        "M25.6 22.9C25.7 23 25.8 23 26 23H33C33.6 23 34 22.6 34 22C34 21.8 34 21.7 33.9 21.6L30.4 14.6C30.1 14.1 29.5 13.9 29 14.2C28.9 14.3 28.7 14.4 28.6 14.6L25.1 21.6C24.9 22 25.1 22.6 25.6 22.9ZM29.5 17.2L31.4 21H27.6L29.5 17.2ZM18.5 14C16 14 14 16 14 18.5C14 21 16 23 18.5 23C21 23 23 21 23 18.5C23 16 21 14 18.5 14ZM18.5 21C17.1 21 16 19.9 16 18.5C16 17.1 17.1 16 18.5 16C19.9 16 21 17.1 21 18.5C21 19.9 19.9 21 18.5 21ZM22.7 25.3C22.3 24.9 21.7 24.9 21.3 25.3L18.5 28.1L15.7 25.3C15.3 24.9 14.7 24.9 14.3 25.3C13.9 25.7 13.9 26.3 14.3 26.7L17.1 29.5L14.3 32.3C13.9 32.7 13.9 33.3 14.3 33.7C14.7 34.1 15.3 34.1 15.7 33.7L18.5 30.9L21.3 33.7C21.7 34.1 22.3 34.1 22.7 33.7C23.1 33.3 23.1 32.7 22.7 32.3L19.9 29.5L22.7 26.7C23.1 26.3 23.1 25.7 22.7 25.3ZM33 25H26C25.4 25 25 25.4 25 26V33C25 33.6 25.4 34 26 34H33C33.6 34 34 33.6 34 33V26C34 25.4 33.6 25 33 25ZM32 32H27V27H32V32Z",
-        :fill "#00d1b2"}]
-      [:circle {:cx "24", :cy "24", :r "23.5", :stroke "#00d1b2"}]]]
-    [:div
-     {:class "ml-3"}
-     [:h4
-      {:class "is-size-4 has-text-weight-bold mb-2"}
-      "Lorem ipsum dolor"]
-     [:p
-      {:class "subtitle has-text-grey"}
-      "Pellentesque eu quam vitae mi lacinia consequat quis in metus."]]]])
+   [:h2
+    {:class "is-size-4 has-text-weight-bold mb-2"}
+    "Players"]
+   [new-player-component game-state]
+
+   (let [players (get-players @game-state)]
+     (for [{:keys [name brains]} players]
+       [:div
+        {:class "mb-6 is-flex" :key (random-uuid)}
+        [:span
+         [:svg
+          {:width "48",
+           :height "48",
+           :viewBox "0 0 48 48",
+           :fill "none",
+           :xmlns "http://www.w3.org/2000/svg"}
+          [:path
+           {:d
+            "M25.6 22.9C25.7 23 25.8 23 26 23H33C33.6 23 34 22.6 34 22C34 21.8 34 21.7 33.9 21.6L30.4 14.6C30.1 14.1 29.5 13.9 29 14.2C28.9 14.3 28.7 14.4 28.6 14.6L25.1 21.6C24.9 22 25.1 22.6 25.6 22.9ZM29.5 17.2L31.4 21H27.6L29.5 17.2ZM18.5 14C16 14 14 16 14 18.5C14 21 16 23 18.5 23C21 23 23 21 23 18.5C23 16 21 14 18.5 14ZM18.5 21C17.1 21 16 19.9 16 18.5C16 17.1 17.1 16 18.5 16C19.9 16 21 17.1 21 18.5C21 19.9 19.9 21 18.5 21ZM22.7 25.3C22.3 24.9 21.7 24.9 21.3 25.3L18.5 28.1L15.7 25.3C15.3 24.9 14.7 24.9 14.3 25.3C13.9 25.7 13.9 26.3 14.3 26.7L17.1 29.5L14.3 32.3C13.9 32.7 13.9 33.3 14.3 33.7C14.7 34.1 15.3 34.1 15.7 33.7L18.5 30.9L21.3 33.7C21.7 34.1 22.3 34.1 22.7 33.7C23.1 33.3 23.1 32.7 22.7 32.3L19.9 29.5L22.7 26.7C23.1 26.3 23.1 25.7 22.7 25.3ZM33 25H26C25.4 25 25 25.4 25 26V33C25 33.6 25.4 34 26 34H33C33.6 34 34 33.6 34 33V26C34 25.4 33.6 25 33 25ZM32 32H27V27H32V32Z",
+            :fill "#00d1b2"}]
+          [:circle {:cx "24", :cy "24", :r "23.5", :stroke "#00d1b2"}]]]
+        [:div
+         {:class "ml-3"}
+         [:h4
+          {:class "is-size-4 has-text-weight-bold mb-2"}
+          name]
+         [:p
+          {:class "subtitle has-text-grey"}
+          "has eaten " [:span {:class "has-text-primary"} brains] " brains."]]]))])
 
 (defn mobile-component []
   [:div
-   {:class "column is-4 is-block-desktop is-hidden-touch"}
-   [:img
-    {:class "mx-auto mt-0-widescreen is-block image",
-     :src "../images/app-page/images/devices/phone-full.svg",
-     :alt ""}]])
+   {:class "column is-12 is-4-desktop"}
+   [:h2
+    {:class "is-size-4 has-text-weight-bold mb-2"}
+    [show-current-hand game-state]]])
 
 (defn footer-component []
   [:footer
@@ -494,7 +511,7 @@
     [:div.columns.is-multiline
      [current-player-component game-state]
      [mobile-component]
-     [lorum-component]]]
+     [list-players-component game-state]]]
    [footer-component]
    [:section.section
     [:div.buttons
